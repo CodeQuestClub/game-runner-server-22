75608bc2b73939d4cefa257a712d03372b94d5b9
Ali Toosi <alirezakhoshghalb@ymail.com>
2022-04-18 23:30:48 +1000

    Timezone to logs

--- app/logs.py
+++ app/logs.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+from pytz import timezone
 
 
 def log(msg):
     print(msg)
-    current_time = datetime.now().strftime("%b %d - %H:%M:%S")
+    current_time = datetime.now(timezone('Australia/Melbourne')).strftime("%b %d - %H:%M:%S")
     with open('logs.txt', 'a') as f:
         f.write(f'{current_time} | {msg}\n')
